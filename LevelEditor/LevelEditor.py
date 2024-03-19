c4157fb0bac27e77d627e9e67c820e0ba7259f5b
dxia2p <d.xia111@gmail.com>
2024-03-19 09:45:36 -0600

    added tile placing still broken though

--- LevelEditor/LevelEditor.py
+++ LevelEditor/LevelEditor.py
@@ -4,23 +4,35 @@ import Gui
 import sys
 pygame.init()
 
-
-tiles = [] # array to store all the active tiles so we don't need to go through the entire grid to see which tiles need to be drawn
-class Tile:
+class Tile: # class to store information about tile at position in grid
+    tiles = []
+    def __init__(self, position, tileType):
+        self.position = position
+        self.tileType = tileType
+        self.tiles.append(self)
+    def drawTile(self):
+        self.tileType.camera.drawTexture(self.tileType.texture, self.position * self.tileType.GRID_SIZE, pygame.Vector2(self.tileType.GRID_SIZE, self.tileType.GRID_SIZE))
+    
+    @staticmethod
+    def drawAll():
+        for t in Tile.tiles:
+            t.drawTile()
+class TileType: # this is for the gui of each different tile and stores the id for each one
     selectedTile = None
-    def onClick(self):
-        if Tile.selectedTile == self:
-            Tile.selectedTile = None
-        else:
-            Tile.selectedTile = self
-    def __init__(self, texture, id):
-        tiles.append(self)
-        self.button = Gui.Button(pygame.Vector2(50 * len(tiles), 500), pygame.Vector2(50, 50), tile1Img, gui, self.onClick)
-        self.texture = texture
-        self.id = id
-    def draw(self):
-        pass
+    tiles = []
 
+    def onClick(self):
+        if TileType.selectedTile == self:
+            TileType.selectedTile = None
+        else:
+            TileType.selectedTile = self
+    def __init__(self, texture, id, camera, GRID_SIZE):
+        TileType.tiles.append(self)
+        self.texture = texture
+        self.button = Gui.Button(pygame.Vector2(50 * len(TileType.tiles), 500), pygame.Vector2(50, 50), self.texture, gui, self.onClick)
+        self.id = id
+        self.camera = camera
+        self.GRID_SIZE = GRID_SIZE
 class Camera:
     def __init__(self, screenSize : pygame.Vector2, screen) -> None:
         self.pos = pygame.Vector2(0, 0)
@@ -41,17 +53,22 @@ class Camera:
         return mousePos
     def drawTexture(self, texture, pos, size = pygame.Vector2(-1, -1)):
         if size != pygame.Vector2(-1, -1):
-            texture = pygame.transform.scale(Tile.selectedTile.texture, size)
+            texture = pygame.transform.scale(texture, size)
         screen.blit(texture, pos + pygame.Vector2(-self.pos.x, self.pos.y) + pygame.Vector2(self.size.x / 2, self.size.y / 2))
 
 screen = pygame.display.set_mode([800, 600])
 running = True
 
+# Grid constants
+GRID_SIZE = 30
+GRID_ROW_COUNT = 101
+GRID_COLUMN_COUNT = 102
+
 camera = Camera(pygame.Vector2(800, 600), screen)
 gui = Gui.GUI(screen)
 tile1Img = pygame.image.load('img/dirtBlock.jpg')
 tile1Img = pygame.transform.scale(tile1Img, (50, 50))
-tile1 = Tile(tile1Img, 0)
+tile1 = TileType(tile1Img, 0, camera, GRID_SIZE)
 tilemap = [[-1] * 100] * 100
 for i in range(0, len(tilemap)):
     for j in range(0, len(tilemap[i])):
@@ -59,9 +76,7 @@ for i in range(0, len(tilemap)):
 
 clock = pygame.time.Clock()
 deltaTime = 0 # time in seconds between each frame
-gridSize = 30
-gridRowCount = 101
-gridColumnCount = 102
+
 #-------------------- MAIN LOOP -------------------------
 clock.tick(30)
 while running:
@@ -81,27 +96,30 @@ while running:
         camera.pos.x -= 5 * deltaTime
 
     # draw a preview of the tile at the mouse poisition if it is selected
-    if Tile.selectedTile != None:
+    if TileType.selectedTile != None:
         p = pygame.mouse.get_pos()
         mousePos = pygame.Vector2(p[0], p[1])
         mousePos = camera.getWorldMousePos(mousePos)
-        mousePos = pygame.Vector2(round(mousePos.x / gridSize) * gridSize, round(mousePos.y / gridSize) * gridSize) - pygame.Vector2(gridSize / 2, gridSize / 2)
+        mousePos = pygame.Vector2(round(mousePos.x / GRID_SIZE) * GRID_SIZE, round(mousePos.y / GRID_SIZE) * GRID_SIZE) - pygame.Vector2(GRID_SIZE / 2, GRID_SIZE / 2)
+        selectedTileGridPos = pygame.Vector2(round(mousePos.x / GRID_SIZE), round(mousePos.y / GRID_SIZE))
         #print("MOUSE POS: " + str(mousePos.x) + " " + str(mousePos.y))
         #print(str(tileZone.x) + " " + str(tileZone.y))
         camera.drawTexture(tile1Img, mousePos, pygame.Vector2(30, 30))
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Tile(selectedTileGridPos, TileType.selectedTile)
 
     # draw the grid
-    width = gridColumnCount * gridSize
-    height = gridRowCount * gridSize
-    for i in range(0, gridColumnCount + 1):
-        camera.drawLine("black", pygame.Vector2(i * gridSize, 0) - pygame.Vector2(gridSize / 2, gridSize / 2), pygame.Vector2(i * gridSize, height) - pygame.Vector2(gridSize / 2, gridSize / 2), 2)
-    for i in range(0, gridRowCount + 1):
-        camera.drawLine("black", pygame.Vector2(0, i * gridSize) - pygame.Vector2(gridSize / 2, gridSize / 2), pygame.Vector2(width, i * gridSize) - pygame.Vector2(gridSize / 2, gridSize / 2), 2)
+    width = GRID_COLUMN_COUNT * GRID_SIZE
+    height = GRID_ROW_COUNT * GRID_SIZE
+    for i in range(0, GRID_COLUMN_COUNT + 1):
+        camera.drawLine("black", pygame.Vector2(i * GRID_SIZE, 0) - pygame.Vector2(GRID_SIZE / 2, GRID_SIZE / 2), pygame.Vector2(i * GRID_SIZE, height) - pygame.Vector2(GRID_SIZE / 2, GRID_SIZE / 2), 2)
+    for i in range(0, GRID_ROW_COUNT + 1):
+        camera.drawLine("black", pygame.Vector2(0, i * GRID_SIZE) - pygame.Vector2(GRID_SIZE / 2, GRID_SIZE / 2), pygame.Vector2(width, i * GRID_SIZE) - pygame.Vector2(GRID_SIZE / 2, GRID_SIZE / 2), 2)
 
     gui.drawElements()
     gui.checkInput(events)
-    for i in tiles:
-        i.draw()
+    Tile.drawAll()
 
     # Did the user click the window close button?
     for event in events:
