1781fd122050712fc7c2fb14d1953e0fbddae951
dxia2p <d.xia111@gmail.com>
2024-04-25 09:23:04 -0600

    debugged saving tilemap system and began working on loading the compressed tilemap

--- LevelEditor/SaveSystem.py
+++ LevelEditor/SaveSystem.py
@@ -24,6 +24,8 @@ def SaveTilemap(tilemap, path = "TileFiles/tilemap.json"):
     tilemapFile = open(path, "w")
     outputList = []
 
+    outputList.append({"IsCompressed": False})
+
     for i in range(len(tilemap)):
         for j in range(len(tilemap[i])):
             if(tilemap[i][j] != None):
@@ -50,31 +52,39 @@ def SaveTilemapCompressed(tilemap, path="TileFiles/tilemap.json"):
     tilemapFile = open(path, "w")
     outputList = []
 
+    outputList.append({"IsCompressed": True})
+
     for i in range(len(tilemap)):
         for j in range(len(tilemap)):
             if not visited[i][j] and tilemap[i][j] != None:
                 targetId = tilemap[i][j].tileTemplate.id
                 startTile = [i, j]
-                targetTile = [i, j] # first move x down until not possible, then move y right until not possible
-                while tilemap[targetTile[0]][targetTile[1]] != None and tilemap[targetTile[0]][targetTile[1]].tileTemplate.id == targetId and not visited[i][j]:
-                    visited[targetTile[0]][targetTile[1]] = True
+                targetTile = [i, j] # first move x right until not possible, then move y down until not possible
+                while tilemap[targetTile[0] + 1][targetTile[1]] != None and tilemap[targetTile[0] + 1][targetTile[1]].tileTemplate.id == targetId and (not visited[targetTile[0] + 1][targetTile[1]]):
+                    visited[targetTile[0] + 1][targetTile[1]] = True
                     targetTile[0] += 1
-                # now expand right
+                print(f"Target tile: {targetTile}, Start Tile: {startTile}")
+                # now expand down
                 canProceed = True
                 while canProceed:
+                    canProceed = True
                     for k in range(startTile[0], targetTile[0] + 1):
                         if tilemap[k][targetTile[1] + 1] == None or tilemap[k][targetTile[1] + 1].tileTemplate.id != targetId or visited[k][targetTile[1] + 1]:
                             canProceed = False
+
                     if canProceed:
+                        targetTile[1] += 1
                         for k in range(startTile[0], targetTile[0] + 1):
                             visited[k][targetTile[1]] = True
-                        targetTile[1] += 1
+
                 rectDict = {
                     "StartPosition": [startTile[0], startTile[1]],
                     "EndPosition": [targetTile[0], targetTile[1]],
                     "Id": targetId
                 }
+
                 outputList.append(rectDict)
+
     jsonObject = json.dumps(outputList, indent=2)
     try:
         tilemapFile.write(jsonObject)
@@ -91,8 +101,13 @@ def LoadTileTemplates(path = "TileFiles/tileTemplates.json"):
     tileTemplatesFile.close()
     return parsedJsonData
 
-def LoadTilemap(path = "TileFiles/tilemap.json"):
+def LoadTilemap(path = "TileFiles/tilemap.json"): # Data is parsed in LevelEditor.py
     tilemapFile = open(path, "r")
     parsedJsonData = json.loads(tilemapFile.read())
     tilemapFile.close()
     return parsedJsonData
+
+def LoadTilemapCompressed(path = "TileFiles/tilemap.json"): # Data is parsed in LevelEditor.py
+    tilemapFile = open(path, "r")
+
+    pass
